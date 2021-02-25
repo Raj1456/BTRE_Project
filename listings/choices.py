a046bdcaaf103eeb39d18f4a4f27b1d730b5e85d
Raju <rajagopalraju6@gmail.com>
2021-02-25 17:30:32 +0530

    First commit

---
+++ listings/choices.py
@@ -0,0 +1,22 @@
+bedroom_choices={
+    '1':1,
+    '2':2,
+    '3':3,
+    '4':4,
+    '5':5,
+    '6':6
+
+}
+
+price_choices={
+    '5000':'5,000',
+    '6000':'6,000',
+    '7000':'7,000',
+}
+
+state_choices={
+    'Kerala':'Kerala',
+    'Tamil Nadu':'Tamil Nadu',
+    'Karnataka':'Karnataka'
+
+}
